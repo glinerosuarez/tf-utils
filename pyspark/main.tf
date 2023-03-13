71f523e6eff9c228b987789a62122d184db508b6
Gabriel Linero <c.gabriel.linero@uptake.com>
2023-03-12 21:25:18 -0500

    pyspark working

--- pyspark/main.tf
+++ pyspark/main.tf
@@ -7,15 +7,24 @@ terraform {
 }
 
 resource "docker_image" "pyspark" {
-  name         = "apache/spark-py"
+  name = "pyspark"
   keep_locally = false
+  build {
+    context = path.module
+  }
+  triggers = {
+    docker_file_sha1 = filesha1("${path.module}/Dockerfile")
+  }
 }
 
 resource "docker_container" "pyspark" {
   image = docker_image.pyspark.image_id
   name  = "pyspark"
   tty = true
-  command = ["/opt/spark/bin/pyspark"]
-  stdin_open = true
+  command = ["python"]
   must_run = false
+  ports {
+    internal = 4040
+    external = 4040
+  }
 }
